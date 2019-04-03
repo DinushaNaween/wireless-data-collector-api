4621e31a98a667b0e67793ee87692f002007c5fa
Dinusha Naveen <dldndasanayaka@gmail.com>
2019-04-03 15:51:22 +0530

    edit app.js

--- app.js
+++ app.js
@@ -1,41 +1,23 @@
 const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const mongoose = require('mongoose');
-const config = require('./config/config');
-
-// Routes files
-const Node = require('./routes/node');
-
-mongoose.connect(config.database);
-//on Connection
-mongoose.connection.on('connected',function(){
-    console.log('connected to databae : ' + config.database);
-    
-});
-//on Error
-mongoose.connection.on('error',function(err){
-    console.log('Database error' + err);
-    
-});
-
 const app = express();
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-//Port Nmber
-const port = 5000;
+const nodeRoutes = require('./routes/node');
 
-// cors middleware
-app.use(cors()); 
+mongoose.connect('mongodb://admin:admin12345@ds019756.mlab.com:19756/wireless-data-collector');
+// mongoose.connect('mongodb://localhost:27017/ClzMate');
 
-//set static folder
-app.use (express.static(path.join(__dirname,'public')));
-
-
-// body-parser middleware
-app.use(bodyParser.urlencoded({extended:false}));  
-app.use(bodyParser.json())
+mongoose.Promise = global.Promise;
+ 
+app.use(morgan('dev'));  
+app.use(bodyparser.urlencoded({extended:false})); 
+app.use(bodyparser.json());  
+ 
+app.use(passport.initialize()); 
+app.use(passport.session());  
 
 app.use((req, res, next)=>{
     res.header("Access-Control-Allow-Origin","*");
@@ -50,15 +32,28 @@ app.use((req, res, next)=>{
     next();
 });
 
-// passport middle ware
-// app.use(passport.initialize());
-// app.use(passport.session());
-// require('./config/passport')(passport);
-
-//Set Node
-app.use('/node',Node);
-
-//start server
-app.listen(port,()=>{
-    console.log('server started on port' + port);
+app.get('/', (req, res, next) => {
+    console.log("Hello Server");
+    res.status(200).json({
+        state: true
+    })
 })
+
+app.use('/node', nodeRoutes);
+
+app.use((req, res, next)=>{
+    const error = new Error('Not Found');
+    error.status(404);
+    next(error);
+});
+
+app.use((error, req, res, next)=>{  
+    res.status(error.status || 500);
+    res.json({ 
+        error: {
+            message: error.message
+        }
+    });
+}); 
+
+module.exports = app;
