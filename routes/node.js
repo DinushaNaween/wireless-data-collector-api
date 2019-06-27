f9ea9dcb01996c152a22aa25beb11efb63c885ac
Dinusha Naveen <dldndasanayaka@gmail.com>
2019-06-27 16:16:36 +0530

    test route edited

--- routes/node.js
+++ routes/node.js
@@ -64,7 +64,7 @@ router.post('/addNewCollector',function(req,res,next){
 });
 
 router.post('/test', (req, res, next) => {
-    console.log(reqbody);
+    console.log(req.body);
     res.status(200).json({
         Msg: "Test Route..."
     });
