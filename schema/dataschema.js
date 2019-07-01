2c2c3d12583165347073af6fa9409d8464c43f6b
Dinusha Naveen <dldndasanayaka@gmail.com>
2019-07-01 17:18:32 +0530

    edit data schema

--- schema/dataschema.js
+++ schema/dataschema.js
@@ -1,12 +1,26 @@
 const mongoose = require('mongoose');
 
 const dataStructure = mongoose.Schema({
-    nodes: [{
-        node_no: { type: String },
-        node_data_01: { type: String },
-        node_data_02: { type: String },
+    node0: {
+        data0: { type: String },
+        data1: { type: String },
         retry: { type: String }
-    }],
+    },
+    node1: {
+        data0: { type: String },
+        data1: { type: String },
+        retry: { type: String }
+    },
+    node2: {
+        data0: { type: String },
+        data1: { type: String },
+        retry: { type: String }
+    },
+    nod3: {
+        data0: { type: String },
+        data1: { type: String },
+        retry: { type: String }
+    },
     centralName: { type: String },
     date: { type: Date },
     time: { type: String },
